dd28a4130753bd6e7e3addce901c77b1d3782978
Rosalie Newman <rosalienewman0@gmail.com>
2023-04-03 20:05:53 -0400

    horrible animations on Home, just done for demo'ing purposes.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Home = () => {
+  // put motion. in front of whatever element we want to animate
+  // give it an animate prop, with an object.
   return (
-    <div className="home container">
-      <h2>
+    <motion.div
+      animate={{ opacity: 0.2, martinTop: 200, rotateZ: 180 }}
+      className="home container"
+    >
+      <motion.h2
+        animate={{ fontSize: "50px", color: "#ff2994", x: 100, y: -100 }}
+      >
         Welcome to Pizza Joint
-      </h2>
+      </motion.h2>
       <Link to="/base">
-        <button>
+        <motion.button animate={{ scale: 1.5 }}>
           Create Your Pizza
-        </button>
+        </motion.button>
       </Link>
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
 export default Home;
